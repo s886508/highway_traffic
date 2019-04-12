36f15950ec0dc0f70c8ff81bea0bbd447be703fd
Will Huang <s951553@gmail.com>
2019-04-12 13:16:05 +0800

    Refine decompression logic.

--- data/decompression.py
+++ data/decompression.py
@@ -11,17 +11,23 @@ def decompress(file_path, output_folder):
     :return: True if success.
     """
     if not os.path.exists(file_path):
-        raise FileNotFoundError
+        raise FileNotFoundError(file_path)
+
+    file_name = os.path.basename(file_path)[:-3]  # Remove '.gz'
+    output_file_path = os.path.join(output_folder, file_name)
+    if os.path.exists(output_file_path):
+        return True
 
     exe_path = '"C:\\Program Files\\7-Zip\\7z.exe"'
-    cmd = '%s e -o%s %s' % (exe_path, output_folder, file_path)
+    cmd = '%s e -y -o%s %s' % (exe_path, output_folder, file_path)
     os.system(cmd)
     return True
 
 
 def decompress_all_data():
+    data_folder = os.path.dirname(os.path.abspath(__file__))
     for date, name in data_name_gen():
-        file_path = os.path.join(os.getcwd(), 'road_level_data', date, name)
-        output_folder = os.path.join(os.getcwd(), 'road_level_data_raw', date)
+        file_path = os.path.join(data_folder, 'road_level_data', date, name)
+        output_folder = os.path.join(data_folder, 'road_level_data_raw', date)
         decompress(file_path, output_folder)
 
